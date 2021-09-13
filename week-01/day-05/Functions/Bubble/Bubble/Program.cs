c7796c104ec6df0dacdbb5284df0d517555c254f
Janek Tichý <janek.tichy@email.cz>
2021-09-13 09:02:58 +0200

    almost finished everything

---
+++ week-01/day-05/Functions/Bubble/Bubble/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Bubble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 25, 47, 12, 15, 8, 1, 5, 32, 60 };
+            sortArray(myArray);
+        }
+        static void sortArray(int[] array)
+        {
+            int isBigger = 0;
+            int[] sorted = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (array[i]>array[j])
+                    {
+                        isBigger++;
+                        
+
+                    }
+                    
+                }
+                sorted[isBigger] = array[i];
+               
+                isBigger = 0;
+            }
+            for (int i = 0; i < sorted.Length; i++)
+            {
+                Console.WriteLine(sorted[i]);
+            }
+
+        }
+    }
+}
---
+++ week-01/day-05/PalindromeSearcher/PalindromeSearcher/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace PalindromeSearcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string str = "012wxyz789";
+            char[] arr;
+
+            
+            string[] words = { "racecar", "dog", "goat", "dad", "duck", "doodle", "never" };
+            PalindromeSearcher(words);
+            
+        }
+        static void PalindromeSearcher(string[] array)
+        {
+            int countOfChars = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    countOfChars++;
+                }
+            }
+            int k = 0;
+            char[] arrayOfChars = new char[countOfChars];
+            for (int i = 0; i < array.Length; i++)
+            {
+                
+                    string currentString = array[i];
+                    char[] parsedWord = currentString.ToCharArray(0, array[i].Length);
+                for (int j = 0; j < parsedWord.Length; j++)
+                {
+                    arrayOfChars[k] = parsedWord[j];
+                    k++;
+                }
+                
+            }
+
+
+            for (int i = 1; i < arrayOfChars.Length - 1; i++)
+            {
+                if ((arrayOfChars[i - 1]) == (arrayOfChars[i + 1]))
+                {
+                    Console.Write(arrayOfChars[i - 1]);
+                    Console.Write(arrayOfChars[i]);
+                    Console.Write(arrayOfChars[i+1]);
+                    Console.WriteLine();
+                }
+            }
+            for (int i = 2; i < arrayOfChars.Length - 2; i++)
+            {
+                if ((arrayOfChars[i - 1]) == (arrayOfChars[i + 1]) && (arrayOfChars[i-2] == arrayOfChars[i+2]))
+                {
+                    Console.Write(arrayOfChars[i - 2]);
+                    Console.Write(arrayOfChars[i - 1]);
+                    Console.Write(arrayOfChars[i]);
+                    Console.Write(arrayOfChars[i + 1]);
+                    Console.Write(arrayOfChars[i + 2]);
+                    Console.WriteLine();
+                }
+            }
+            for (int i = 3; i < arrayOfChars.Length - 3; i++)
+            {
+                if ((arrayOfChars[i - 1]) == (arrayOfChars[i + 1]) && (arrayOfChars[i - 2] == arrayOfChars[i + 2]) && (arrayOfChars[i - 3] == arrayOfChars[i + 3]))
+                {
+                    Console.Write(arrayOfChars[i - 3]);
+                    Console.Write(arrayOfChars[i - 2]);
+                    Console.Write(arrayOfChars[i - 1]);
+                    Console.Write(arrayOfChars[i]);
+                    Console.Write(arrayOfChars[i + 1]);
+                    Console.Write(arrayOfChars[i + 2]);
+                    Console.Write(arrayOfChars[i - 3]);
+                    Console.WriteLine();
+                }
+            }
+
+        }
+    }
+}
