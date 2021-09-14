7f5b3156d27d9f3a81aa144e8c1edde0c66d60c2
Janek Tichý <janek.tichy@email.cz>
2021-09-14 11:26:43 +0200

    make more complex file program

---
+++ week-02/day-02/CountEachLine/CountEachLine/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace CountEachLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "/textfile.txt";
+
+            try
+            {
+                string[] fileLines = File.ReadAllLines(fileName);
+                int fileLineCount = fileLines.Length;
+                Console.WriteLine("The file has " + fileLineCount + " Lines");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("There is no text in the file");
+                throw;
+            }
+
+
+        }
+    }
+}
---
+++ week-02/day-02/PrintEachLine/PrintEachLine/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace PrintEachLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"my-file.txt";
+            try
+            {
+                
+                string[] content = File.ReadAllLines(path);
+                Console.WriteLine(content[0]);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unable to read file: my-file.txt");
+            }
+
+            Console.ReadLine();
+           
+        }
+    }
+}
