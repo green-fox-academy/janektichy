61f77819d5e90f1baef229c4a82129a3a8dcbec2
Janek Tichý <janek.tichy@email.cz>
2021-09-15 10:36:47 +0200

    last few exercises which doesn't work for now, I will return to them later

---
+++ week-02/day-02/LotteryNumbers/LotteryNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace LotteryNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numbersToSort = "numbers.txt";
+            SortNumbers(numbersToSort);
+        }
+        static void SortNumbers(string fileName)
+        {
+            string[] converted = File.ReadAllLines(fileName);
+            string[][] splited = new string[converted.Length][];
+            string[] splitedAndShorted = new string[splited.Length];
+            for (int i = 0; i < converted.Length; i++)
+            {
+                splited[i] = converted[i].Split(";");
+            }
+            
+            for (int i = 0; i < splited.Length-1; i++)
+            {
+                for (int j = splited[i].Length-1; j >= splited[i].Length-5; j--)
+                {
+                    splitedAndShorted[i] = splited[i][j];
+                    Console.Write(splitedAndShorted[i]);
+                }
+                Console.WriteLine();
+            }
+           
+
+
+        }
+    }
+}
