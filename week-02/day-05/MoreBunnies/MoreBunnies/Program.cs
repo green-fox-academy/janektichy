b5ac8796bc9557e4629652a86772de0d8f0de76d
Janek Tichý <janek.tichy@email.cz>
2021-09-16 16:33:29 +0200

    make recursive function that takes number of bunnies and returns sum of their ears (considered every odd bunny has 3 ears)

--- week-02/day-05/MoreBunnies/MoreBunnies/Program.cs
+++ week-02/day-05/MoreBunnies/MoreBunnies/Program.cs
@@ -6,7 +6,23 @@ namespace MoreBunnies
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(CountEars(10));
+        }
+        static int CountEars(int bunnies, int EarCounter = 0)
+        {
+            if (bunnies == 0)
+            {
+                return EarCounter;
+            }
+            if (bunnies%2 == 0)
+            {
+                return CountEars(bunnies - 1, EarCounter += 2);
+            }
+            else
+            {
+                return CountEars(bunnies - 1, EarCounter+=3);
+            }
+
         }
     }
 }
