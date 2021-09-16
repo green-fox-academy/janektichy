7111c06cc4612d78c107ba02d46485290619ce4f
Janek Tichý <janek.tichy@email.cz>
2021-09-16 15:15:04 +0200

    Make function without loops, that takes a number and returns sum of its digits

---
+++ week-02/day-05/SumDigit/SumDigit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SumDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumOfDigits(2328221));
+        }
+        static int SumOfDigits(int number)
+        {
+            if(number < 10)
+            {
+                return number;
+            }
+            return number%10 + SumOfDigits(number / 10);
+        }
+    }
+}
