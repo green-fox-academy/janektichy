db770f8ca66205bfc3307d1ba3e04dc8c491dda7
Janek Tichý <janek.tichy@email.cz>
2021-09-16 15:25:54 +0200

    make function that takes a two numbers and returns the value of the first to the power of the second

---
+++ week-02/day-05/Power/Power/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Power(2,8));
+        }
+        static int Power(int number, int power)
+        {
+            if (power == 1)
+            {
+                return number;
+            }
+            return number * Power(number, power - 1);
+        }
+    }
+}
