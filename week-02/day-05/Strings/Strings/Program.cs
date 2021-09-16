293898fad6a3315f3cee2ed1e19340a5b6604727
Janek Tichý <janek.tichy@email.cz>
2021-09-16 17:33:38 +0200

    make recursive function that changes all x chars in a string to y chars

---
+++ week-02/day-05/Strings/Strings/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ChangeXchars("jsdsjkxsjdkxsdxxasdjlxjxkjxjx"));
+        }
+        static string ChangeXchars(string text, int i = 0)
+        {
+            if (i == text.Length)
+            {
+                return text;
+            }
+            else if (text[i] == 'x')
+            {
+                char[] converted = text.ToCharArray();
+                converted[i] = 'y';
+                text = new string(converted);
+                return ChangeXchars(text, ++i);
+            }
+            else
+            {
+                return ChangeXchars(text, ++i);
+            }
+
+        }
+    }
+}
