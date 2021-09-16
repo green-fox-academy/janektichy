4fc89adf9305f49ebcc3109cfe75358f7f8f153a
Janek Tichý <janek.tichy@email.cz>
2021-09-16 16:27:23 +0200

    Make few basic recursive function exercises

---
+++ week-02/day-05/Bunnies/Bunnies/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Bunnies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NumberOfEars(20));
+        }
+        static int NumberOfEars(int bunnies, int EarCount = 0)
+        {
+
+            if (bunnies > 0)
+            {
+                return NumberOfEars(bunnies - 1, EarCount+=2);
+            }
+            return EarCount;
+            
+        }
+    }
+}
---
+++ week-02/day-05/GreatestCommonDivisor/GreatestCommonDivisor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GreatestCommonDivisor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BiggestDivisor(60, 115));
+        }
+        static int BiggestDivisor(int firstNumber, int secondNumber)
+        {
+            if (secondNumber != 0)
+            {
+                return BiggestDivisor(secondNumber, firstNumber % secondNumber);
+            }
+            else
+            {
+                return firstNumber;
+            }
+        }
+    }
+}
