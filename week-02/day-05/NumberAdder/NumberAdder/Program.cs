97222969eb5618625e723b4037c9b44655d99644
Janek Tichý <janek.tichy@email.cz>
2021-09-16 15:02:06 +0200

    start recursion, make function that takes a number and returns sum of that number to 1

---
+++ week-02/day-05/NumberAdder/NumberAdder/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NumberAdder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NumberAdder(4));
+        }
+        static int NumberAdder(int lastNumber)
+        {
+            if(lastNumber == 1)
+            {
+                return 1;
+            }
+            return lastNumber + NumberAdder(lastNumber-1);
+        }
+    }
+}
