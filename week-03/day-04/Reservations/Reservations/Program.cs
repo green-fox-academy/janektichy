5753b80010e7bc95417fcb89870f8a0ddc50f627
Janek Tichý <janek.tichy@email.cz>
2021-09-23 14:54:16 +0200

    make a simple reservation app using interface

---
+++ week-03/day-04/Reservations/Reservations/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Reservations
+{
+    class ReservationApp
+    {
+        static void Main(string[] args)
+        {
+            Reservation reservation1 = new Reservation();
+
+            string codeBooked = reservation1.GetCodeBooking();
+            string dayBooked = reservation1.GetDowBooking();
+
+            Console.WriteLine($"Booking# {codeBooked} for {dayBooked}");
+        }
+    }
+}
---
+++ week-03/day-04/Reservations/Reservations/Reservation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservations
+{
+    class Reservation : IReservationy
+    {
+        protected string[] dow;
+        protected char[] code;
+
+        public Reservation()
+        {
+            this.dow = new string[] { "MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN" };
+            this.code = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+        }
+        public string GetCodeBooking()
+        {
+            char[] generatedCode = new char[8];
+            Random random = new Random();
+            for (int i = 0; i < 8; i++)
+            {
+                generatedCode[i] = code[random.Next(0, this.code.Length)];
+            }
+            string codeToString = new string(generatedCode);
+            return codeToString;
+        }
+
+        public string GetDowBooking()
+        {
+            Random random = new Random();
+            return this.dow[random.Next(1, 7)];
+        }
+    }
+}
